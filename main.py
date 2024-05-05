c33a8bed68f9dc9a43770663d0212a1f5a7d4902
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-05-05 16:05:57 +0530

    Added some functions and Project's Concept added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 
 def getAPIkey():
 
-    with open("./WeatherAPI_Python/API_KEY/api_key.dat", "rb") as f:
+    with open("C:/Users/shubh/OneDrive/Desktop/PythonProj/WeatherAPI_Python/API_KEY/api_key.dat", "rb") as f:
 
         try:
             while True:
@@ -22,12 +22,22 @@ def getAPIkey():
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
 API_KEY = getAPIkey()
 
-CITY = "bhilai"
+
+def ConvertTemp(KelvinValue):
+
+    CELSIUS = (KelvinValue - 273.15)
+    FAHRENHEIT = ((KelvinValue - 273.15) * (1.8) + 32)
+
+    return {"CELSIUS": CELSIUS , "FAHRENHEIT": FAHRENHEIT}
+
+
+CITY = "Bhilai"
+
 
 URL = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
 
 response = requests.get(URL).json()
 
-print(response)
-# print(response['name'])
-# print(response['sys'])
+temp_kelvin = response['main']['temp']
+
+print(ConvertTemp(temp_kelvin))
